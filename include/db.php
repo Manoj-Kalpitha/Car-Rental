7ccb2194f0a1aedf69eab47a9618bd504bce8a83
Manoj-Kalpitha <manojkalpitha010@gmail.com>
2024-10-25 21:51:03 +0530

    add sql file

--- include/db.php
+++ include/db.php
@@ -1,7 +1,7 @@
 <?php
 $serverName = "localhost:3307";  // Update to "localhost" if default port is used
 $dbUsername = "root";
-$dbPassword = "123456";
+$dbPassword = "1234";
 $dbName = "car_rental";
 
 // Create connection
@@ -18,4 +18,3 @@ if (!$conn) {
 
 // Close connection when done
 // mysqli_close($conn);
-?>

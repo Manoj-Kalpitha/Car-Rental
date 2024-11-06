19d23539afe192690d7ac4b01d84bcf9131b9447
Shanuz12 <shaninikapitiya@gmail.com>
2024-11-07 00:42:22 +0530

    add some comments

--- carlist.php
+++ carlist.php
@@ -5,6 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Car Rental - Car Listing</title>
+
+  <!-- External CSS  -->
   <link rel="stylesheet" href="css/style.css">
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -14,8 +16,6 @@
   <?php
   include_once "./nav.php";
   ?>
-
-
 
   <!-- filter*******************************************************-->
 

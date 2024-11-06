0f183f29a16595a3c86266eb272559f41595b686
Shanuz12 <shaninikapitiya@gmail.com>
2024-11-07 00:40:56 +0530

    separated navigation and footer

---
+++ nav.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navclr p-3">
+    <div class="container-fluid">
+        <a class="navbar-brand navclra" href="#"><img src="./Assest/img/logo_v4.png" class=" navimg rounded float-start" alt="..."></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-4">
+                <li class="nav-item ">
+                    <a class="nav-link active navclra" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link navclra" href="about.php">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link navclra" href="carlist.php">Car Listing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link navclra" href="contact.php">Contact Us</a>
+                </li>
+            </ul>
+            <div class="d-flex align-items-center gap-2 px-4 ">
+                <button class="btn border-0 text-center profile_btn" data-bs-toggle="offcanvas" data-bs-target="#offcanvasProfile" aria-controls="offcanvasProfile">
+                    <i class="bi bi-person-square text-white fs-4" data-lucide="circle-user-round"></i>
+                </button>
+
+                <!-- <div class="d-flex ms-4" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              </div>
+              <button class="button1" type="submit">Search</button> -->
+                </form>
+            </div>
+        </div>
+</nav>

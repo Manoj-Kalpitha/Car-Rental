23b40086bf9332310ac4ecfbb0f7d75a39c175c6
Shanuz12 <shaninikapitiya@gmail.com>
2024-11-07 01:58:22 +0530

    update sql

---
+++ sql/Dump20241107.sql
@@ -0,0 +1,264 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: car_rental
+-- ------------------------------------------------------
+-- Server version	8.0.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `car_availability`
+--
+
+DROP TABLE IF EXISTS `car_availability`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `car_availability` (
+  `car_availability_id` int NOT NULL AUTO_INCREMENT,
+  `car_id` int DEFAULT NULL,
+  `unavailable_from` date NOT NULL,
+  `unavailable_to` date NOT NULL,
+  `reason` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`car_availability_id`),
+  KEY `car_id` (`car_id`),
+  CONSTRAINT `car_availability_ibfk_1` FOREIGN KEY (`car_id`) REFERENCES `cars` (`car_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `car_availability`
+--
+
+LOCK TABLES `car_availability` WRITE;
+/*!40000 ALTER TABLE `car_availability` DISABLE KEYS */;
+/*!40000 ALTER TABLE `car_availability` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `car_maintenance`
+--
+
+DROP TABLE IF EXISTS `car_maintenance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `car_maintenance` (
+  `maintenance_id` int NOT NULL AUTO_INCREMENT,
+  `car_id` int DEFAULT NULL,
+  `maintenance_date` date NOT NULL,
+  `description` text,
+  `cost` decimal(10,2) DEFAULT NULL,
+  `service_center` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`maintenance_id`),
+  KEY `car_id` (`car_id`),
+  CONSTRAINT `car_maintenance_ibfk_1` FOREIGN KEY (`car_id`) REFERENCES `cars` (`car_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `car_maintenance`
+--
+
+LOCK TABLES `car_maintenance` WRITE;
+/*!40000 ALTER TABLE `car_maintenance` DISABLE KEYS */;
+/*!40000 ALTER TABLE `car_maintenance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cars`
+--
+
+DROP TABLE IF EXISTS `cars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cars` (
+  `car_id` int NOT NULL AUTO_INCREMENT,
+  `make` varchar(50) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  `year` int NOT NULL,
+  `registration_no` varchar(20) NOT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  `seating_capacity` int DEFAULT NULL,
+  `fuel_type` varchar(20) DEFAULT NULL,
+  `daily_rate` decimal(10,2) NOT NULL,
+  `status` enum('Available','Rented','Maintenance') DEFAULT 'Available',
+  `image_url` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`car_id`),
+  UNIQUE KEY `registration_no` (`registration_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cars`
+--
+
+LOCK TABLES `cars` WRITE;
+/*!40000 ALTER TABLE `cars` DISABLE KEYS */;
+INSERT INTO `cars` VALUES (1,'Toyota','Corolla',2021,'ABC1234','Sedan',5,'Gasoline',50.00,'Available','test.jpg','2023-10-01 10:30:00'),(2,'Honda','Civic',2022,'DEF5678','Sedan',5,'Gasoline',55.00,'Available','test.jpg','2023-10-02 12:00:00'),(3,'Ford','Escape',2020,'GHI9012','SUV',7,'Diesel',70.00,'Maintenance','test.jpg','2023-10-03 09:00:00'),(4,'Chevrolet','Tahoe',2021,'JKL3456','SUV',8,'Gasoline',90.00,'Rented','test.jpg','2023-10-04 11:00:00'),(5,'BMW','X5',2023,'MNO7890','SUV',5,'Diesel',120.00,'Available','test.jpg','2023-10-05 15:00:00'),(6,'Audi','A4',2019,'PQR1122','Sedan',5,'Gasoline',65.00,'Available','test.jpg','2023-10-06 16:30:00'),(7,'Mercedes','C-Class',2022,'STU3344','Sedan',5,'Hybrid',100.00,'Available','test.jpg','2023-10-07 08:45:00'),(8,'Nissan','Altima',2021,'VWX5566','Sedan',5,'Gasoline',48.00,'Rented','test.jpg','2023-10-08 14:20:00'),(9,'Tesla','Model 3',2023,'YZA7788','Sedan',5,'Electric',110.00,'Available','test.jpg','2023-10-09 10:15:00'),(10,'Hyundai','Sonata',2020,'BCD9901','Sedan',5,'Gasoline',47.00,'Maintenance','test.jpg','2023-10-10 12:45:00'),(13,'abc test','2011',221,'aa','aa',11,'Gasoline',11.00,'Available','ssa','2024-11-07 01:43:12');
+/*!40000 ALTER TABLE `cars` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customers` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `locations` (
+  `location_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `zip_code` varchar(10) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`location_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locations`
+--
+
+LOCK TABLES `locations` WRITE;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payments`
+--
+
+DROP TABLE IF EXISTS `payments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payments` (
+  `payment_id` int NOT NULL AUTO_INCREMENT,
+  `reservation_id` int DEFAULT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `payment_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `payment_method` enum('Credit Card','PayPal','Cash','Bank Transfer') DEFAULT NULL,
+  `status` enum('Completed','Pending') DEFAULT 'Pending',
+  PRIMARY KEY (`payment_id`),
+  KEY `reservation_id` (`reservation_id`),
+  CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`reservation_id`) REFERENCES `reservations` (`reservation_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payments`
+--
+
+LOCK TABLES `payments` WRITE;
+/*!40000 ALTER TABLE `payments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservations` (
+  `reservation_id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int DEFAULT NULL,
+  `car_id` int DEFAULT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `total_cost` decimal(10,2) NOT NULL,
+  `status` enum('Active','Completed','Canceled') DEFAULT 'Active',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`reservation_id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `car_id` (`car_id`),
+  CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
+  CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`car_id`) REFERENCES `cars` (`car_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservations`
+--
+
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `user_type` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'user',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (2,'Manoj','manoj@gmail.com','202cb962ac59075b964b07152d234b70','admin'),(5,'Dimuth','dimuth@gmail.com','202cb962ac59075b964b07152d234b70','user');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-07  1:57:37
